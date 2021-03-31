2729df7ec55685fb26e3c0315dca3cd0d29e9d10
walterpacheco <walter.e.pacheco.chandia@gmail.com>
2021-03-31 10:08:47 -0300

    Se crea data.js para limpiar App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Jumbotron from "./componentes/Jumbotron";
 import Navbar from "./componentes/Navbar";
 import Card from "./componentes/Card";
 import Footer from "./componentes/Footer";
+import {data} from "./data.js"
 
 function App() {
   let objectNavbar = {
@@ -11,46 +12,6 @@ function App() {
     services: "Services",
     contact: "Contact",
   };
-
-  let data = [
-    {
-      imgenUrl: "https://picsum.photos/id/237/400/300",
-      imgenAlt: "perro",
-      title: "Soy un Perro",
-      cardBody:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      btnUrl: "#",
-      btnLabel: "Go somewhere",
-    },
-    {
-      imgenUrl: "https://picsum.photos/id/236/400/300",
-      imgenAlt: "perro",
-      title: "Soy un Perro",
-      cardBody:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      btnUrl: "#",
-      btnLabel: "Go somewhere",
-    },
-    {
-      imgenUrl: "https://picsum.photos/id/238/400/300",
-      imgenAlt: "perro",
-      title: "Soy un Perro",
-      cardBody:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      btnUrl: "#",
-      btnLabel: "Go somewhere",
-    },
-    {
-      imgenUrl: "https://picsum.photos/id/235/400/300",
-      imgenAlt: "perro",
-      title: "Soy un Perro",
-      cardBody:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      btnUrl: "#",
-      btnLabel: "Go somewhere",
-    },
-  ];
-
   let footerProps = {
     tituloFooter: "Copyright",
     webSite: "your website 2019",
